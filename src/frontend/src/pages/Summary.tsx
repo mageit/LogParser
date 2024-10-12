b207f7e7439a399fefe487c0818f38405c3850d5
Jack Zhong <jzhong@dius.com.au>
2024-10-12 15:50:37 +1100

    Update some spelling mistake.

--- src/frontend/src/pages/Summary.tsx
+++ src/frontend/src/pages/Summary.tsx
@@ -4,7 +4,7 @@ import LogParserUploader from "@/components/LogParserUploader";
 const Home: React.FC = (): React.ReactElement => {
   return (
     <section>
-        <h1>Log Parser Summaruy</h1>
+        <h1>Log Parser Summary</h1>
         <p>To begin the analysis, please upload a log file in a <strong>.log</strong> format</p>
         <p>Once you've uploaded a compatible log file, the analysis process will commence automatically.</p>
         <hr /><br />
